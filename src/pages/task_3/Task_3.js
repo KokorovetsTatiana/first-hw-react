5ccb94b20a074a22c6abf4ff8e4d34ed4e2c5e7b
KokorovetsTatiana <tanuska@ukr.net>
2021-06-28 21:39:24 +0300

    finished task 3

--- src/pages/task_3/Task_3.js
+++ src/pages/task_3/Task_3.js
@@ -1,10 +1,12 @@
-import React, { Component } from "react"
+import React, { Component } from "react";
+import FriendList from './friendList.js';
+import friends from './friends.json';
 
 export default class Task_3 extends Component {
     render() {
         return (
             <div className="center" id="task3">
-                This is task 3
+                <FriendList friends={friends} />
             </div>
         )
     }
